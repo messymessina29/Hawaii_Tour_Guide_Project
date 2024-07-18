7f9ba15a0b1e4eb7b08c3ec6f06397ae4e5af74a
Andrew <andrew.t.messina@gmail.com>
2024-07-17 21:07:02 -0700

    Added drop down function to restaurant map

--- Andrew/hawaii_map.js
+++ Andrew/hawaii_map.js
@@ -1,46 +1,85 @@
+// Variable to hold the map and markers
+let hawaii_map;
+let allMarkers = L.layerGroup();
+let street;
+
 // Function to create the map with base layers and overlays
-function createMap(restaurants) {
+function createMap() {
     // Create the base layer
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    // Create a baseMaps object
-    let baseMaps = {
-        "Street Map": street,
-    };
-
-    // Create an overlay object to hold our overlay
-    let overlayMaps = {
-        "Restaurants": restaurants
-    };
-
-    // Create the map
-    let hawaii_map = L.map("map", {
+    // Create the map of Hawaii
+    hawaii_map = L.map("map", {
         center: [21.3099, -157.8581],
         zoom: 13,
-        layers: [street, restaurants]
+        layers: [street]
     });
 
     // Add layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
+    L.control.layers({
+        "Street Map": street
+    }, {}, {
         collapsed: false
     }).addTo(hawaii_map);
 }
 
 // Load the restaurant data and create markers
-d3.json('restaurants.json').then(function(data) {
+d3.json('Resources/hawaii_restaurants.json').then(function(data) {
     // Create a layer group for restaurant markers
-    let restaurantMarkers = L.layerGroup();
-
     data.forEach(function(d) {
         // Create a marker for each restaurant
         let marker = L.marker([d.Latitude, d.Longitude]).bindPopup(
-            '<b>' + d.Name + '</b><br>Type: ' + d.Type + '<br>Address: ' + d.Address + '<br>Distance: ' + d['Distance(Miles)'] + ' miles'
+            '<b>' + d.Name + '</b><br>Type: ' + d.Type + '<br>Yelp Rating: ' + d.Rating + '<br>Price: ' + d.Price + '<br>Address: ' + d.Address + '<br>Distance: ' + d['Distance(Miles)'] + ' miles'
         );
-        restaurantMarkers.addLayer(marker);
+        marker.type = d.Type;  // Add a custom property to the marker
+        allMarkers.addLayer(marker);
     });
 
-    // Call the createMap function with the restaurant markers
-    createMap(restaurantMarkers);
+    // Add the restaurant markers to the map
+    allMarkers.addTo(hawaii_map);
+
+    // Populate the dropdown
+    selectTypes(data);
+
+    // Store the initial data for filtering
+    window.initialData = data;
 });
+
+// Function to populate the dropdown with unique restaurant types
+function selectTypes(data) {
+    // Extract unique restaurant types
+    let restTypes = [...new Set(data.map(d => d.Type))];
+
+    // Select the dropdown element
+    let selector = d3.select('#selType');
+
+    // Add an "All" option
+    selector.append('option').text("All").property('value', "All");
+
+    // Append options to the dropdown
+    restTypes.forEach(type => {
+        selector.append('option').text(type).property('value', type);
+    });
+}
+
+// Function to handle dropdown selection change
+function optionChanged(selectedType) {
+    // Clear all existing layers from the map except the base layer and street layer
+    hawaii_map.eachLayer(function(layer) {
+        if (layer !== street && layer !== hawaii_map.tileLayer) {
+            hawaii_map.removeLayer(layer);
+        }
+    });
+
+    // Filter markers based on the selected type and add them to the map
+    allMarkers.eachLayer(function(marker) {
+        if (selectedType === "All" || marker.type === selectedType) {
+            hawaii_map.addLayer(marker);
+        }
+    });
+}
+
+// Initialize the map
+createMap();
